ede3c7d6bb24d4d9436244f90a0140c10536f22b
tkmagesh <tkmagesh77@gmail.com>
2015-11-19 13:22:29 +0530

    Building Task Manager - 2

--- Day-02/11-taskManager/routes/taskRoutes.js
+++ Day-02/11-taskManager/routes/taskRoutes.js
@@ -16,4 +16,18 @@ router.get('/new', function(req, res, next){
     res.render('tasks/new', {title : 'New Task'});
 });
 
+router.post('/new', function(req, res, next){
+    var taskName = req.body.txtTask,
+        taskId = taskStore.reduce(function(result, task){
+            return result > task.id ? result : task.id;
+        },0)+1;
+    var newTask = {
+        id : taskId,
+        name : taskName,
+        isCompleted : false
+    };
+    taskStore.push(newTask);
+    res.redirect('/tasks');
+});
+
 module.exports = router;
